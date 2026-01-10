639109b0f8202d3bae579b818529074df16c222e
Iamsushantgautam <sushantkumarlko2507@gmail.com>
2026-01-10 20:47:49 +0530

    connect

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,17 @@ const connectDB = require('./config/db');
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://filmy-cosmo-admin-dashboard.vercel.app", "http://localhost:5173"], // Add your Vercel URL here
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
 
 app.get('/', (req, res) => res.send('Movie app backend'));
 
